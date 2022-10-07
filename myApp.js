97fea3671cb0eebcf7d79728f49312121691005a
Jose <jose48130@gmail.com>
2022-10-07 21:22:36 +0000

    Update 3 files

--- myApp.js
+++ myApp.js
@@ -1,6 +1,25 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+//console.log("Hello World");
+/*
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+*/
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//console.log(response);
+const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
+app.get("/json", (req, res) => {
+  //let response = "Hello json".toUpperCase();
+  res.json({
+    message:
+      MESSAGE_STYLE == process.env.MESSAGE_STYLE ? "HELLO JSON" : "Hello json",
+  });
+});
 module.exports = app;
